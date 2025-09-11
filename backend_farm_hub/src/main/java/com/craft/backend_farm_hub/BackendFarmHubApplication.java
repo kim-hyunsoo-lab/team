586450334661951b40db6a593465f54cdf016fbe
kim <nus100472@gmail.com>
2025-09-11 15:27:25 +0900

    farmHub 프로젝트 생성

---
+++ backend_farm_hub/src/main/java/com/craft/backend_farm_hub/BackendFarmHubApplication.java
@@ -0,0 +1,13 @@
+package com.craft.backend_farm_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFarmHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFarmHubApplication.class, args);
+	}
+
+}
---
+++ backend_farm_hub/src/test/java/com/craft/backend_farm_hub/BackendFarmHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craft.backend_farm_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFarmHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
