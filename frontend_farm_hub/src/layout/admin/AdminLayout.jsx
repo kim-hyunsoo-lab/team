56acc5f5bf709ddabfc9ba4f62a430baa5e72250
ne-aki <geunhyeong930@gmail.com>
2025-09-15 14:00:31 +0900

    관리자 레이아웃

--- frontend_farm_hub/src/layout/admin/AdminLayout.jsx
+++ frontend_farm_hub/src/layout/admin/AdminLayout.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import styles from './AdminLayout.module.css'
+import { Outlet } from 'react-router'
+import AdminHeader from './AdminHeader'
+import AdminSide from './AdminSide'
 
 const AdminLayout = () => {
   return (
-    <div>AdminLayout</div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <AdminHeader />
+      </div>
+      <div className={styles.body}>
+        <div className={styles.side_menu}>
+          <AdminSide />
+        </div>
+        <div className={styles.content}>
+          <Outlet />
+        </div>
+      </div>
+    </div>
   )
 }
 
