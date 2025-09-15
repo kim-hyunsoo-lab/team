67037876108de54c7136c5287d188db22d0991e9
silunicis <silunicis@gmail.com>
2025-09-15 10:33:22 +0900

    로그인 기능

--- frontend_farm_hub/src/layout/user/UserHeader.jsx
+++ frontend_farm_hub/src/layout/user/UserHeader.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './UserHeader.module.css'
-import Input from '../../common/Input'
 import { NavLink, useNavigate } from 'react-router'
+import Login from '../components/Login'
+import Input from '../common/Input'
 
 const UserHeader = () => {
-
   const nav = useNavigate();
+
+  // loginInfo 데이터 가져오기
+  const loginInfo = sessionStorage.getItem('loginInfo');
+  const loginInfoData = JSON.parse(loginInfo);
+
+  // 로그인 Modal 창 숨김/보이기 여부
+  const [isOpenLogin, setIsOpenLogin] = useState(false);
+  
+  // 회원가입 Modal 창 숨김/보이기 여부
+
 
   return (
     <div className={styles.container}>
@@ -15,10 +25,24 @@ const UserHeader = () => {
           즐겨찾기
         </p>
         <ul>
-          <li>로그인</li>
-          <li>회원가입</li>
-          <li>장바구니</li>
-          <li>주문/배송조회</li>
+          {
+            loginInfo ?
+            <>
+              <li>
+                <span>{loginInfoData.memId}님 환영합니다</span>
+              </li>
+              <li>장바구니</li>
+              <li>주문/배송조회</li>
+              <li onClick={e => {sessionStorage.removeItem('loginInfo'); nav(`/`);}}>로그아웃
+
+              </li>
+            </>
+            :            
+            <>
+              <li onClick={()=>setIsOpenLogin(true)}>로그인</li>
+              <li>회원가입</li>
+            </>
+          }
           <li>고객센터</li>
         </ul>
       </div>
@@ -59,6 +83,14 @@ const UserHeader = () => {
           </li>
         </ul>
       </div>
+    
+    {/* 로그인 Modal */}
+    <Login isOpenLogin={isOpenLogin}
+      onClose={()=>setIsOpenLogin(false)}/>
+    {/* 회원가입 Modal */}
+
+
+
     </div>
   )
 }
