7db30d94adf33676bc31c669e6657e6a40be647d
ne-aki <geunhyeong930@gmail.com>
2025-09-15 15:39:21 +0900

    관리자 레이아웃 작업

--- frontend_farm_hub/src/common/Input.jsx
+++ frontend_farm_hub/src/common/Input.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import styles from './Input.module.css'
 
-const Input = ({size='300px', ...props}) => {
+const Input = ({size='300px', color='brown', ...props}) => {
   return (
     <div
-      className={styles.common_input}
+      className={`${styles.common_input} ${styles[color]}`}
       style={{
         width : size
       }}
       {...props}
     >
-      <input type="text" />
+      <input type="text" {...props} />
       <span><i className="bi bi-search"></i></span>
     </div>
   )
