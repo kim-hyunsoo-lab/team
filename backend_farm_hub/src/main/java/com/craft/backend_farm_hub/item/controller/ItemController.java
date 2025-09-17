7f4335211e85b6e2e6f60f1f98514a4e29181abb
silunicis <silunicis@gmail.com>
2025-09-17 17:09:38 +0900

    이미지 등록 기능 만드는 중임

--- backend_farm_hub/src/main/java/com/craft/backend_farm_hub/item/controller/ItemController.java
+++ backend_farm_hub/src/main/java/com/craft/backend_farm_hub/item/controller/ItemController.java
@@ -1,4 +1,40 @@
 package com.craft.backend_farm_hub.item.controller;
 
+import com.craft.backend_farm_hub.item.dto.ItemDTO;
+import com.craft.backend_farm_hub.item.dto.ItemImgDTO;
+import com.craft.backend_farm_hub.item.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.FileUpload;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+//    import com.craft.backend_farm_hub.util.FileUploadUtil;
+
+import java.io.File;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("items")
 public class ItemController {
+  private final ItemService itemService;
+
+// //진행중
+//  public void insertItem(
+//          @RequestParam("mainImg") MultipartFile mainImg,
+//          @RequestParam(name = "subImgs", required = false) MultipartFile[] subImgs,
+//          ItemDTO itemDTO
+//  ) {
+//    ItemImgDTO imgDTO = FileUploadUtil.fileUpload(mainImg);
+//    List<ItemImgDTO> imgList = FileUploadUtil.multipleFileUpload(subImgs);
+//            imgList.add(imgDTO);}
+
+
+
+
+
+
 }
--- backend_farm_hub/src/main/java/com/craft/backend_farm_hub/item/service/ItemService.java
+++ backend_farm_hub/src/main/java/com/craft/backend_farm_hub/item/service/ItemService.java
@@ -1,4 +1,27 @@
 package com.craft.backend_farm_hub.item.service;
 
+import com.craft.backend_farm_hub.item.dto.ItemDTO;
+import com.craft.backend_farm_hub.item.dto.ItemImgDTO;
+import com.craft.backend_farm_hub.item.mapper.ItemMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ItemService {
+  private final ItemMapper itemMapper;
+
+  public void insertItem(ItemDTO itemDTO, List<ItemImgDTO> itemImgList){
+    int nextItemNum = itemMapper.getNextItemNum();
+    itemDTO.setItemNum(nextItemNum);
+
+    for (ItemImgDTO dto : itemImgList){
+      dto.setItemNum(nextItemNum);
+    }
+    itemMapper.insertItem(itemDTO);
+    itemMapper.insertImgs(itemImgList);
+  }
+
 }
