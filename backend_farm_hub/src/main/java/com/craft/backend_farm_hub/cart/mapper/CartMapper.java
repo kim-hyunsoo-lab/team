9a8b003f4d0914da9eb834847b44a1de308961bf
ne-aki <geunhyeong930@gmail.com>
2025-09-25 16:50:57 +0900

    상품상세보기 일부 수정, 장바구니 관련 기능 구현 중

--- backend_farm_hub/src/main/java/com/craft/backend_farm_hub/cart/mapper/CartMapper.java
+++ backend_farm_hub/src/main/java/com/craft/backend_farm_hub/cart/mapper/CartMapper.java
@@ -1,4 +1,9 @@
 package com.craft.backend_farm_hub.cart.mapper;
 
+import com.craft.backend_farm_hub.cart.dto.CartDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface CartMapper {
+  public void insertCart(CartDTO cartDTO);
 }
