b6718cc1f4fd2711b687f688f5f977ad7289f007
ne-aki <geunhyeong930@gmail.com>
2025-09-25 11:06:11 +0900

    상품 상세보기 코드 수정

--- frontend_farm_hub/src/pages/user/products/NewProductList.jsx
+++ frontend_farm_hub/src/pages/user/products/NewProductList.jsx
@@ -3,37 +3,15 @@ import styles from './NewProductList.module.css'
 import PageTitle from '../../../common/PageTitle'
 import { useNavigate } from 'react-router'
 import Menu from '../../../components/Menu'
+import NewProducts from '../../../components/ProductList/NewProducts'
 
-const NewProductList = ({newProducts}) => {
+const NewProductList = () => {
   const nav = useNavigate();
 
-  console.log(newProducts)
   return (
     <div className={styles.container}>
       <Menu />
-      <PageTitle title='신상품' />
-        <div className={`${styles.grid_div}`}>
-          {
-            newProducts.map((newProduct, i) => {
-              
-              return (
-                <div
-                  className={styles.grid_content}
-                  key={i}
-                  onClick={e => nav(`/product-detail/${newProduct.itemNum}/intro`)}
-                >
-                  <div className={styles.grid_img}>
-                    <img src={`http://localhost:8080/upload/${newProduct.imgList[0].attachedImgName}`} />
-                  </div>
-                  <div className={styles.grid_info}>
-                    <h3>{newProduct.itemName}</h3>
-                    <p>{newProduct.price.toLocaleString()}원</p>
-                  </div>
-                </div>
-              )
-            })
-          }
-        </div>
+      <NewProducts />
     </div>
   )
 }
