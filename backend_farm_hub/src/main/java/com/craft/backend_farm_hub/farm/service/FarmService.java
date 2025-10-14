85c58a0eaf6ba98b913d4d433d8a36d7a34afe7e
kim <nus100472@gmail.com>
2025-10-14 10:45:04 +0900

    차트 완료 후 최종 수정을 위한 합병

--- backend_farm_hub/src/main/java/com/craft/backend_farm_hub/farm/service/FarmService.java
+++ backend_farm_hub/src/main/java/com/craft/backend_farm_hub/farm/service/FarmService.java
@@ -22,11 +22,12 @@ public class FarmService {
   }
 
   //축사 조도 데이터 조회
-  public List<Integer> getIlluminanceData(int[] each){
+  public List<Integer> getIlluminanceData(int[] each) {
     return farmMapper.getIlluminanceData(each);
+  }
 
+  //축사 공기질 데이터 조회
   public List<Integer> getAirQualityData(int[] each) {
     return farmMapper.getAirQualityData(each);
-
   }
 }
