c0610fbe8d0ccea0ef8209380cb16293dd00ad63
kim <nus100472@gmail.com>
2025-10-21 15:26:52 +0900

    관리자 레이아웃 완성

--- frontend_farm_hub_app/app/(tabs)/alarm/index.jsx
+++ frontend_farm_hub_app/app/(tabs)/alarm/index.jsx
@@ -1,16 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 // app/alarm/index.jsx
 // alarm 탭을 터치하면 실행되는 페이지
-const AlarmHomeScreen = () => {
+const AlarmScreen = () => {
   return (
-    <View>
-      <Text>AlarmHomeScreen 입니다</Text>
-    </View>
+    <SafeAreaView>
+      <Text>AlarmScreen 입니다</Text>
+    </SafeAreaView>
   )
 }
 
-export default AlarmHomeScreen
+export default AlarmScreen
 
 const styles = StyleSheet.create({})
